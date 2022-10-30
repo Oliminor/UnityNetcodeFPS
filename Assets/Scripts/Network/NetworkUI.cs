a254751faeb4913a7786389706e550edc1f5fdfd
Joe Simpson <k2056245@kingston.ac.uk>
2022-10-30 08:48:31 +0000

    idk stuff I guess
    wow stuff is great
    I know IP input but is broken

--- Assets/Scripts/Network/NetworkUI.cs
+++ Assets/Scripts/Network/NetworkUI.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using TMPro;
 
 public class NetworkUI : NetworkBehaviour
 {
     [SerializeField] TextMeshProUGUI textInputForPlayerName;
+    [SerializeField] TextMeshProUGUI _IPInput;
+    [SerializeField] TextMeshProUGUI _PortInput;
     public static NetworkUI instance;
 
     public string GetPlayerNameFromInput() { return textInputForPlayerName.text; }
 
     private GameObject _ObjectiveManager;
+    private GameObject _NetworkManager;
 
     void Start()
     {
         _ObjectiveManager = GameObject.Find("ObjectiveManager");
+        _NetworkManager = GameObject.Find("NetworkManager");
         instance = this;
         Application.targetFrameRate = 120;
+    }
+
+    void Update()
+    {
+        _NetworkManager.GetComponent<UnityTransport>().ConnectionData.Address = _IPInput.text;
     }
 
     public void StartHost()
